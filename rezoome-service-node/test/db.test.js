48498d894fcb83fec7303fc815280fe1954e147b
안택수(An Tacksu) <ahnzaz@gmail.com>
2017-11-10 22:40:50 +0900

    DB Test case

--- rezoome-service-node/test/db.test.js
+++ rezoome-service-node/test/db.test.js
@@ -11,60 +11,60 @@ var mongoose = require('mongoose');
 var User = require('../module/db/user_model');
 var Record = require('../module/db/record_model');
 
-describe('User DB Test', ()=>{
-it('Create user', (done)=>{
-    var myUser = new User({
-        name : 'Jackson',
-        phoneNo : '010-4545-8787'
-    });
-    
-    myUser.save().then((user)=>{
-        assert.equal(myUser.name, user.name);
-        assert.equal(myUser.phoneNo, user.phoneNo);
-        done();
-    });
-});
-
-it('Delete User', ()=>{
-    var myUser = new User({
-        name : 'Patrik Jane',
-        phoneNo : '0108-484645'
-    });
-
-    return myUser.save().then((user)=>{
-        assert.equal(user.name, myUser.name);
-        assert.equal(user.phoneNo, myUser.phoneNo);
-
-        user.remove().then((removedUser)=>{
-            assert(user.name, "ASDFASDF");
+describe('User DB Test', () => {
+    it('Create user', (done) => {
+        var myUser = new User({
+            name: 'Jackson',
+            phoneNo: '010-4545-8787'
         });
-    })
-});
-});
 
-describe('User add record', ()=>{
-    it('Add a record to user', (done)=>{
-    var myUser = new User({
-        name : 'MName',
-        phoneNo : '1019654654'
+        myUser.save().then((user) => {
+            assert.equal(myUser.name, user.name);
+            assert.equal(myUser.phoneNo, user.phoneNo);
+            done();
+        });
     });
 
-    myUser.addRecord(new Record({
-        type : 'Opic',
-        grade : 'IH',
-        date : Date.now()
-    }))
+    it('Delete User', () => {
+        var myUser = new User({
+            name: 'Patrik Jane',
+            phoneNo: '0108-484645'
+        });
 
-    myUser.save().then((user)=>{
-        assert(user.records[0].type, 'Opic');
-        assert(user.records[0].grade, 'IH');
-        done();
-    })
+        return myUser.save().then((user) => {
+            assert.equal(user.name, myUser.name);
+            assert.equal(user.phoneNo, myUser.phoneNo);
+
+            user.remove().then((removedUser) => {
+                assert(user.name, "ASDFASDF");
+            });
+        })
+    });
+});
+
+describe('User add record', () => {
+    it('Add a record to user', (done) => {
+        var myUser = new User({
+            name: 'MName',
+            phoneNo: '1019654654'
+        });
+
+        myUser.addRecord(new Record({
+            type: 'Opic',
+            grade: 'IH',
+            date: Date.now()
+        }))
+
+        myUser.save().then((user) => {
+            assert(user.records[0].type, 'Opic');
+            assert(user.records[0].grade, 'IH');
+            done();
+        })
     });
 })
 
-describe('Test', function(){
-    it('Hi!', function(){
-        assert.equal(0, 0);
+describe('Disconnect database', ()=>{
+    it('Disconnect database', ()=>{
+        mongoose.disconnect();
     });
-});
+})
