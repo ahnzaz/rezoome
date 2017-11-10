79d5591ce71e92492cecca77eaeafcc00d8d8a31
안택수(An Tacksu) <ahnzaz@gmail.com>
2017-11-11 00:16:42 +0900

    Blockchain testcode

--- rezoome-service-node/module/blockchain/blockchain.js
+++ rezoome-service-node/module/blockchain/blockchain.js
@@ -1,25 +1,28 @@
-var http = require('http');
+var https = require('https');
 var fs = require('fs');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const nexledgerServer = {
 
-	hostname : 'https://112.106.145.78',
+	hostname : '112.106.145.78',
 	port : '18080',
 	method : 'POST',
 	
 	headers : {
-		'Content-Type' : 'application/json;charset=utf-8', 
+		'Content-Type' : 'application/json'
 	},
 
-	ca : [fs.readFileSync('../../ca/nexledger_node.cer')]
+	ca : [fs.readFileSync('../ca/nexledger_node.cer')]
 }
 
 var blockchain = {
 	getRecord : (data, callback) => {
-	    http.request(nexledgerServer, (response)=>{
+	    var req = https.request(nexledgerServer, (response)=>{
 	        response.on('data', callback);
-	    });
-				
+		});
+		
+		req.write(JSON.stringify(data));
 	}
 }
 
--- rezoome-service-node/test/blockchain.test.js
+++ rezoome-service-node/test/blockchain.test.js
@@ -7,11 +7,8 @@ describe('Blockchain IO Test', function(){
 	it('OPIc record IO', function(done){
 		blockchain.getRecord({
 			'phoneNo' : '010-1234-1234'
-		}, function(err, data){
-			for(var record in data){
-				assert.equal('010-1234-1234', record.phoneNo);
-				done();
-			}
+		}, (data)=>{
+			console.log(data.toString());
 		})
 	});
 });
