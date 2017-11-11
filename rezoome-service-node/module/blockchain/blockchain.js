992911c4ab5717ccb986a2d68e204c38978435b6
TackSu An <ahnzaz@gmail.com>
2017-11-11 05:36:36 +0000

    Blockchain module test complete

--- rezoome-service-node/module/blockchain/blockchain.js
+++ rezoome-service-node/module/blockchain/blockchain.js
@@ -1,6 +1,8 @@
 var https = require('https');
 var fs = require('fs');
 
+// TODO This would neutralize TLS certificate verification.
+// Remove this code when publish.
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const nexledgerServer = {
@@ -13,16 +15,38 @@ const nexledgerServer = {
 		'Content-Type' : 'application/json'
 	},
 
-	ca : [fs.readFileSync('../ca/nexledger_node.cer')]
+	// TODO Replace valid CA certifiate.
+	// ca : [fs.readFileSync('../ca/nexledger_node.cer')]
 }
 
+/**
+ * Blockchain moduel to access NexLedger node interface. <br />
+ * 
+ * @since 1.0.0
+ * @author TACKSU
+ * @updated 171110 - created.
+ */
 var blockchain = {
-	getRecord : (data, callback) => {
-	    var req = https.request(nexledgerServer, (response)=>{
-	        response.on('data', callback);
+	
+	/**
+	 * Retrieve record for specific user. <br />
+	 * 
+	 * @since 1.0.0
+	 * @author TACKSU
+	 * @updated 171111 - created.
+	 */
+	getRecord : (reqData, callback) => {
+		nexledgerServer.path = '/nexledger_get_all';
+	    var req = https.request(nexledgerServer, (res)=>{
+	        res.on('data', (data)=>{
+	        	callback(null, JSON.parse(data));
+	        	});
 		});
+		req.on('error', (err)=>{
+			callback(err, null);
+		}).write(JSON.stringify(reqData));
 		
-		req.write(JSON.stringify(data));
+		// req.end();
 	}
 }
 
--- rezoome-service-node/test/blockchain.test.js
+++ rezoome-service-node/test/blockchain.test.js
@@ -4,39 +4,52 @@ var blockchain = require('../module/blockchain/blockchain');
 var fs = require('fs');
 var mlog = require('mocha-logger');
 
+var http = require('http');
 var https = require('https');
 const option = {
 	agent : false,
 	hostname : '112.106.145.78',
 	port : '18080',
 	method : 'POST',
+	path : '/nexledger_get_all',
 	
 	headers : {
 		'Content-Type' : 'application/json'
 	},
 
-	ca : [fs.readFileSync('../ca/nexledger_node.cer')]
+	// ca : [fs.readFileSync('../ca/nexledger_node.cer')]
 }
 
 describe('Blockchain IO Test', function(){
 
 	it('Basic https request', function(done){
-		https.request(option, function(res){
+		process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+		var req = https.request(option, function(res){
 			mlog.log('status: ', res.statusCode);
 			mlog.log('headers: ', res.headers);
 			res.on('data', function(data){
-				mlog.log('data');
+				mlog.log('data:', data);
 				done();
 			});
-		})
+		}).on('error', function(err){
+			mlog.error(err.toString());
+		});
+		req.write(JSON.stringify({
+			'phoneNumber' : '010-4564-8787'
+		}));
+		
+		req.end();
 	})
 	
-	// it('OPIc record IO', function(done){
-	// 	blockchain.getRecord({
-	// 		'phoneNumber' : '010-4564-8787'
-	// 	}, (data)=>{
-	// 		mlog.log(data);
-	// 		console.log(data.toString());
-	// 	})
-	// });
+	it('OPIc record IO', function(done){
+		blockchain.getRecord({
+			'phoneNumber' : '010-4564-8787'
+		}, (err, data)=>{
+			if(err)
+				mlog.error(err);
+			else
+				mlog.log(data);
+			done();
+		})
+	});
 });
