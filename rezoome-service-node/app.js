d8c62a2fa900a2ca523aa412115b28a68bbb1273
TackSu An <ahnzaz@gmail.com>
2017-11-12 11:13:08 +0000

    UI fix

--- rezoome-service-node/app.js
+++ rezoome-service-node/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// app.use('/', index);
 
 // API route.
 app.use('/', require('./service/api/route'));
